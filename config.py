8b0d7662e575f38a4c38488c2860c1e6daec0123
clipperjorie-a11y <clipperjorie@gmail.com>
2026-08-20 12:28:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+# Konfigurasi Bot Telegram Utama (Dari @BotFather)
 BOT_TOKEN = "8821336371:AAGcpzvG-9maQz7dXQbWDpcQAQLRqhtnNDo"
 OWNER_ID = 8858059241
+
+# Daftar Harga Paket untuk Menu Toko
+PRICING = {
+    "basic": {"1d": 2000, "1b": 4000, "perm": 15000},
+    "autoreply": {"1d": 3000, "1b": 5000, "perm": 20000},
+    "full": {"1d": 3000, "1b": 7000, "perm": 35000}
+}
