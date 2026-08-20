839eab36c33ee4322d6c9398f56782f1c9b3d2f5
clipperjorie-a11y <clipperjorie@gmail.com>
2026-08-20 11:47:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 
-# Ambil credential Telegram dari https://my.telegram.org
-API_ID = int(os.getenv("API_ID", "1234567")) 
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH_HERE")
+API_ID = int(os.getenv("API_ID", "1234567"))
+API_HASH = os.getenv("API_HASH", "API_HASH_ANDA")
+OWNER_ID = 123456789 # Ganti dengan ID Telegram Anda
 
-# API Orderkuita
-ORDERKUITA_MERCHANT_ID = os.getenv("ORDERKUITA_MERCHANT_ID", "YOUR_MERCHANT_ID")
-ORDERKUITA_API_KEY = os.getenv("ORDERKUITA_API_KEY", "YOUR_API_KEY")
+ORDERKUOTA_MERCHANT_ID = "MERCHANT_ANDA"
+ORDERKUOTA_API_KEY = "API_KEY_ANDA"
+
+# Skema Harga Userbot: "paket": {"1d": harga, "1b": harga, "perm": harga}
+PRICING = {
+    "basic": {"1d": 2000, "1b": 4000, "perm": 15000},
+    "autoreply": {"1d": 3000, "1b": 5000, "perm": 20000},
+    "full": {"1d": 3000, "1b": 7000, "perm": 35000}
+}
